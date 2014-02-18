bc3bc981081eb2584e1352e7390adb2331c33e3f
ufuchs <ufuchs@gmx.com>
2014-02-18 19:46:08 +0100

    removed: unnecessary properties

--- weather-config.js
+++ weather-config.js
@@ -2,15 +2,7 @@
 
 'use strict';
 
-var path = require('path'),
-
-    ds = process.env.NODE_ENV === undefined && process.env.NODE_ENV === 'development'
-        ? 'byTestData'
-        : 'byProvider',
-
-    appCfg = {
-
-        weatherSource : ds,
+var appCfg = {
 
         aws : {
             key: process.env.AWS_ACCESS_KEY_ID,
@@ -19,49 +11,8 @@ var path = require('path'),
         },
 
         mongo : {
-            server : 'mongodb://127.0.0.1:27017',
+            server : 'mongodb://localhost:27017',
             database : 'weather',
-        },
-
-        devices : {
-            kindle4nt : {
-                resolution : '600x800',
-                singleDayDisplay : false,
-                crushed : true,
-                maxForecastDays : 4
-            },
-            df3120 : {
-                resolution : '320x240',
-                singleDayDisplay : true,
-                crushed : true,
-                maxForecastDays : 3
-            }
-        },
-
-        production : {
-
-            dir : './public/weather',
-
-            files : {
-
-                names : {
-                    weatherPng : 'weather'
-                },
-
-                quantity : {
-                    kindle4nt : 1,
-                    df3120 : 4
-                }
-
-            },
-
-            /* expire time in seconds of the output files*/
-            expires : 3600
-
-        },
-
-        templatesPool : {
-            dir : './templates'
         }
 
     };
@@ -73,54 +24,5 @@ module.exports = appCfg;
 //
 appCfg.getMongoConnString = function () {
     return this.mongo.server + '/' + this.mongo.database;
-};
-
-//
-//
-//
-appCfg.getDisplay = function (device) {
-    return {
-        device : device,   // not really needed
-        resolution : appCfg.devices[device].resolution,
-        singleDayDisplay : appCfg.devices[device].singleDayDisplay,
-        crushed : appCfg.devices[device].crushed,
-        maxForecastDays : appCfg.devices[device].maxForecastDays
-    };
-};
-
-//
-//
-//
-appCfg.getProductionDir = function () {
-    return path.resolve(appCfg.production.dir);
-};
-
-//
-//
-//
-appCfg.getSvgTemplate = function (device) {
-    return path.join(path.resolve(appCfg.templatesPool.dir),
-        appCfg.devices[device].resolution + '.svg');
-};
-
-//
-//
-//
-appCfg.getDeviceDir = function (device) {
-    return path.join(path.resolve(appCfg.production.dir), device);
-};
-
-//
-//
-//
-appCfg.getTargetDir = function (device, location) {
-    return path.join(appCfg.getDeviceDir(device), location);
-};
-
-//
-//
-//
-appCfg.getPngFilename = function () {
-    return appCfg.production.files.names.weatherPng;
 };
 
